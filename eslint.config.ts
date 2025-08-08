bfcfdce1178cfe563e6d4622968c3943b5408f14
Jason Lin <zhengdelin0720@gmail.com>
2025-08-08 14:02:45 +0800

    chore: 新增專案配置檔案
    - 新增 .editorconfig 以統一編碼風格
    - 新增 .prettierrc.json 以設定 Prettier 格式化規則
    - 新增 eslint.config.ts 以配置 ESLint 規則
    - 更新 package.json 和 pnpm-lock.yaml，新增相關開發依賴
    
    這些變更有助於提升代碼一致性和可維護性。

---
+++ eslint.config.ts
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  // @ts-ignore
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]);
